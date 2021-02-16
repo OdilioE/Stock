a85aaab1637336019fe17e0ed3c8ee6e8ae19132
OdilioE <40282498+OdilioE@users.noreply.github.com>
2021-02-16 13:20:59 -0600

    Update Products.cs

--- Products.cs
+++ Products.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//modification by Odi today..... go to gibhub
+//modification by Odi today..... go to gibhub and it worked
 
 namespace Stock003
 {
